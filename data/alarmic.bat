4d4329c0a0ce7ee08086328e6bee89c0096c3c02
Nirmal Shane <nirsh@outlook.com>
2012-08-08 16:08:46 +0530

    Setup
    This is the complete Setup. App it self is in 'data' directory.

---
+++ data/alarmic.bat
@@ -0,0 +1,17 @@
+cls
+@echo off
+title Alarmic
+echo ALARMIC
+echo https://github.com/nirsh/Alarmic
+echo 24 hr time (ex: 15:00)
+set /p t=Alarm at ~ 
+cls
+echo Alarm set for %t%.
+
+REM Alarm code:
+:alarm
+	if %time:~0,5%==%t% "%programfiles%\Windows Media Player\wmplayer.exe" /play /close /new "%cd%\alarm.mp3"
+	REM You can use @echo 
+	REM or you can change ...\notify.wav into a different mp3/sound.
+	exit
+goto alarm
---
+++ install-alarmic.bat
@@ -0,0 +1,10 @@
+﻿cls
+@echo off
+
+echo Welcome to Alarmic for Windows!
+echo Installing ...
+
+if not exist %userprofile%\Apps mkdir %userprofile%\Apps
+if not exist %userprofile%\Apps\Alarmic mkdir %userprofile%\Apps\Alarmic
+copy %cd%\data\* %userprofile%\Apps\Alarmic\*
+copy %cd%\data\Alarmic.lnk %userprofile%\desktop\Alarmic.lnk
